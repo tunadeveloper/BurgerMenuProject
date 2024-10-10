9d9ee8e2f5215d0af24bd27da9b919b0cc459d6e
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-10-10 12:02:54 +0300

    Add project files.

---
+++ BurgerMenuProject/Context/BurgerMenuContext.cs
@@ -0,0 +1,17 @@
+﻿using BurgerMenuProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BurgerMenuProject.Context
+{
+    public class BurgerMenuContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<DealOfTheDay> DealOfTheDays { get; set; }
+        public DbSet<Testimonial> testimonials { get; set; }
+    }
+}
---
+++ BurgerMenuProject/Entities/Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BurgerMenuProject.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Customername { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
